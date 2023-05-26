1dddd0a053b681f21dd54f4defee78aabcbac6ec
WhamBamGang <berlinjohneric123@gmail.com>
2023-05-26 21:22:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DICKBUTT-WEBSITE
+ 
---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Suck my dickbutt.");
+
+function changeText() {
+    console.log("Hello World!");
+}
+
+function toggleImage(){
+    let img = document.getElementById('dICKBUTT');
+    if (img.style.display === 'none') {
+        img.style.display = 'block';
+    } else {
+        img.style.display = 'none';
+    }
+}
